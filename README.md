23de1c9b2288836b4475d4eb3aef3b08630af81f
Stavros <stasotiro@gmail.com>
2022-01-29 19:22:55 +0200

    base solution

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # lso_assignment
+
 LSO assignment Winter semester 21
+
+### Model Creation
+
+Creating the model follows the predefined process. The only difference is (apart from the numbers), the creation of a fixed number of Routes, one for each truck, that are passed in the solvers.
+
+### Creating the solution
+
+The idea behind the solution is:
+
+- Sort the customers based on Profit/ServiceTime to get the probably best customers routed first.
+- Iterate over the list until a customer can not fit in any route
+- In each iteration find the best fitting customer, who maximises the profit and has smaller cost. Cost for a customer is the costToVisitCustomer + customerServiceTime.
+- In order for a customer to be considered, the CustomerCost (serviceTime+VisitCost) must be < RouteTimeCapacity
+- After finding the best fit, add him to the route that maximises the value Profit/Cost, with emphasis on Profit (by multiplying with a number)
+- If no customers are found as best fits, the process is finished and we have a complete solution with total profit the sum of profits of each route
---
+++ VRP_Model.py
@@ -0,0 +1,60 @@
+import random
+import math
+
+
+class Model:
+
+# instance variables
+    def __init__(self):
+        self.allNodes = []
+        self.customers = []
+        self.routes = []
+        self.matrix = []
+        self.capacity = -1
+
+    def BuildModel(self):
+        random.seed(5112001)
+        depot = Node(0, 50, 50, 0, 0)
+        self.allNodes.append(depot)
+
+        self.capacity = 150
+        totalCustomers = 30
+        availableTrucks = 5
+        for i in range (0, totalCustomers):
+            x = random.randint(0, 100)
+            y = random.randint(0, 100)
+            service_time = random.randint(5, 10)
+            profit = random.randint(5, 20)
+            cust = Node(i + 1, x, y, profit, service_time)
+            self.allNodes.append(cust)
+            self.customers.append(cust)
+
+        rows = len(self.allNodes)
+        self.matrix = [[0.0 for x in range(rows)] for y in range(rows)]
+        
+        for i in range(0, availableTrucks): 
+            newRoute = Route(self.capacity, depot)
+            self.routes.append(newRoute)
+
+        for i in range(0, len(self.allNodes)):
+            for j in range(0, len(self.allNodes)):
+                a = self.allNodes[i]
+                b = self.allNodes[j]
+                dist = math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
+                self.matrix[i][j] = dist
+
+class Node:
+    def __init__(self, idd, xx, yy, profit, st):
+        self.x = xx
+        self.y = yy
+        self.ID = idd
+        self.service_time = st
+        self.profit = profit
+        self.isRouted = False
+
+class Route:
+    def __init__(self, cap, depot):
+        self.sequenceOfNodes = [depot, depot]
+        self.profit = 0
+        self.capacity = cap
+        self.load = 0
